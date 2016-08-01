54836112674b27372bd8482a49aed33a7f450aa4
brewdamaster <brewdamaster@gmail.com>
2016-08-01 13:50:22 -0400

    Add files via upload

---
+++ ldap_timestamp_unix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+userPath="/Users/$(echo $(id -un))"  # pulls OS X user for dscl read
+fileTime=$(dscl /Active\ Directory/SIGNPOST/All\ Domains -read $userPath | grep 'dsAttrTypeNative:lastLogon:' | awk '{print $2}') #prints stamp, lastLogon in this case
+winSecs=$((($fileTime/10000000))) 
+unixTimestamp=$((($winSecs-11644473600))) # converted value
+unixDate=$(date -r $unixTimestamp)
+echo $unixDate # output
