300a8d24cdcadc1dc90e7d805bfe419c6ebfb3fd
Jordan Brewer <brewdamaster@gmail.com>
2016-08-23 11:05:39 -0400

    small edit

--- ldap_timestamp_unix.sh
+++ ldap_timestamp_unix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 userPath="/Users/$(echo $(id -un))"  # pulls OS X user for dscl read
-fileTime=$(dscl /YOURDIRSERVICE/YOURDOMAIN/All\ Domains -read $userPath | grep 'dsAttrTypeNative:lastLogon:' | awk '{print $2}') #prints stamp, lastLogon in this case
-winSecs=$((($fileTime/10000000))) 
+fileTime=$(dscl /YOURDIRSERVICE/YOURDOMAIN/All\ Domains -read $userPath | grep 'dsAttrTypeNative:lastLogon:' | awk '{print $2}') #prints stamp (lastLogon in this case, rm "/All\ Domains" if not applicable)
+winSecs=$((($fileTime/10000000)))
 unixTimestamp=$((($winSecs-11644473600))) # converted value
 unixDate=$(date -r $unixTimestamp)
 echo $unixDate # output
